ca2dc8ab92d888c41f3333fdcfed2f8dab53c303
TWoolhouse <thomasrwoolhouse@gmail.com>
2021-02-09 20:16:35 +0000

    client server moved to async node system + works

--- client.py
+++ client.py
@@ -1,27 +1,63 @@
-import enum
-import network
-import loader
-import game
 import gui
+import enum
+import game
+import node
+import asyio
+import loader
+import caching
+import functools
+from interface import Interface
+from typing import Callable, Any
 
-class Application:
-    def __init__(self, client):
-        self.client = client
-        self.window = gui.Window("Game Client Menu")
-        PageHome(self.window)
-        PageGame(self.window, self)
-        PageSetting(self.window, self)
+class Requests(node.Client):
+    def __init__(self, addr: str, port: int):
+        super().__init__(addr, port)
+
+    @caching.cache(1, 5)
+    async def download(self, name: str):
+        print("Download:", name)
+        self.send(name, "GAME")
+        data = await self.recv("GAME")
+        if data and data[0].data:
+            loader.write(data[0].data)
+            return True
+        return False
+
+    @caching.cache(1, 5)
+    async def retrieve_list(self):
+        print("Retrieve List")
+        self.send(b"", "GLIST")
+        return [i.data for i in await self.recv(node.Tag("GLIST"), wait=(await self.recv("GLIST"))[0].data)]
+
+
+class GuiApplication:
+
+    def __init__(self, client: 'Client'):
+        self.client: Client = client
+        self.window = gui.Window("Game Client Menu", resolution=10)
+        # Pages
+        PageHome(self.window, self, "home")
+        PageGame(self.window, self, "games")
+        PageLoadGame(self.window, self, "load_game")
+        PageSetting(self.window, self, "settings")
         self.window.show_page("home")
 
+    def call(self, func: Callable[..., Any], *args, **kwargs) -> Any:
+        self.window.call(functools.partial(func, *args, **kwargs))
+    async def acall(self, func: Callable[..., Any], *args, **kwargs) -> Any:
+        fut = Interface.loop.create_future()
+        def guicallback():
+            fut.set_result(func(*args, **kwargs))
+        self.window.call(guicallback)
+        return await fut
+
     def main(self):
-        if self.client.network.is_open():
-            for page in self.window.pages:
-                pass
+        self.window.mainloop()
 
 class AppPage(gui.Page):
-    def __init__(self, parent, app):
-        self.app = app
-        super().__init__(parent, "home")
+    def __init__(self, parent: gui.Window, app: GuiApplication, name: str):
+        self.app: GuiApplication = app
+        super().__init__(parent, name)
 
     def connect(self):
         pass
@@ -37,11 +73,59 @@ class PageHome(AppPage):
 class PageGame(AppPage):
     def setup(self):
         self.add(gui.tk.Label(self, text="Games"), row=1, column=1, pady=10)
-        self.add(gui.tk.Button(self, text="Current: None", command=lambda: None), "current", row=2, column=1, pady=5)
+        self.add(gui.tk.Button(self, text="Current: None", command=self.play_game), "current", row=2, column=1, pady=15)
         self.add(gui.tk.Button(self, text="Return", command=lambda: self.show_page("home")), row=99, column=1, pady=15)
+        self.edit("current", "state", "disabled")
 
-    def update_list(self, games):
-        self.client.update_list()
+    def show(self):
+        Interface.schedule(self.app.client._retrieve_list())
+
+    def update_listing(self):
+        for name in tuple(self.widgets.keys()):
+            if name.startswith("g_"):
+                self.remove(name)
+
+        for row, name in enumerate(sorted(self.app.client.games.names), start=3):
+            self.add(gui.tk.Button(self, text=name.title(), command=gui.cmd(self.app.window["load_game"].load, name)), f"g_{name}", row=row, column=1, pady=2)
+
+    def play_game(self):
+        self.edit("current", "state", "disabled")
+        self.edit("current", "text", "Running...")
+        Interface.schedule(self.app.client._play_game())
+        for name in tuple(self.widgets.keys()):
+            if name.startswith("g_"):
+                self.edit(name, "state", "disabled")
+
+    def return_game(self):
+        self.edit("current", "state", "normal")
+        self.edit("current", "text", f"Current: {self.app.client.games.active}")
+        for name in tuple(self.widgets.keys()):
+            if name.startswith("g_"):
+                self.edit(name, "state", "normal")
+
+class PageLoadGame(AppPage):
+    def setup(self):
+        self.game_name = "None"
+        self.add(gui.tk.Label(self, text="GAME NAME"), "game_name", row=1, column=1, pady=10)
+        self.add(gui.tk.Button(self, text="Download", command=self.load_game), "download", row=2, column=1, pady=5)
+        self.add(gui.tk.Button(self, text="Return", command=lambda: self.show_page("games")), row=99, column=1, pady=15)
+
+    def load(self, name: str):
+        self.edit("game_name", "text", name.title())
+        self.game_name = name
+        if not self.app.client._active.is_set():
+            self.show_page(self.name)
+
+    def show(self):
+        self.edit("download", "state", "normal")
+        self.edit("download", "text", "download")
+
+    def load_game(self):
+        self.parent["games"].edit("current", "state", "disabled")
+        self.edit("download", "state", "disabled")
+        self.edit("download", "text", "download...")
+        print("Load Game:", self.game_name)
+        Interface.schedule(self.app.client._load_game(self.game_name))
 
 class PageSetting(AppPage):
     def setup(self):
@@ -50,39 +134,50 @@ class PageSetting(AppPage):
         self.add(gui.tk.Button(self, text="Return", command=lambda: self.show_page("home")), row=99, column=1)
 
     def show(self):
-        self.edit("server", "text", "Server: " + (self.app.client.network.conn_name() if self.app.client.network.is_open() else "Disconnected"))
+        self.edit("server", "text", "Server: " + (self.app.client.net.addr if self.app.client.net else "Disconnected"))
 
 class Client:
     def __init__(self, addr: str):
+        self.guiapp = GuiApplication(self)
+        self.net = Requests(addr, 609)
+        self.games = loader.GameSet()
+        self.games.names.add("dino")
 
-        # Module
-        self.module = loader.Module()
-
-        # Network Client
-        self.network = network.Client(addr)
-
-        # Window
-        self.application = Application(self)
+        self._active = asyio.Nevent()
 
     def main(self):
+        Interface.schedule(self.net.open())
         try:
-            self.network.open()
-        except network.node.CloseError:
-            print("Connection Error")
-        try:
-            self.application.main()
+            self.guiapp.main()
         finally:
-            self.network.close()
+            Interface.schedule(self.net.close())
 
-    def update_list(self):
-        pass
+    @caching.cache(1, 5)
+    async def _retrieve_list(self) :
+        names = await self.net.retrieve_list()
+        self.games.names.clear()
+        self.games.names.update(names)
+        self.guiapp.call(self.guiapp.window["games"].update_listing)
 
-    def request_game(self, index: int):
-        if self.network.is_open():
-            if self.network.game(self.module.names[index]) and self.module.reload():
-                self.module._active = index
+    async def _load_game(self, name):
+        if await self.net.download(name) and self.games.reload():
+            self.games.active = name
+        self.guiapp.call(self.guiapp.window["games"].edit, "current", "text", f"Current: {self.games.active}")
+        self.guiapp.call(self.guiapp.window["load_game"].edit, "download", "text", "Done!")
+        if self.games.valid():
+            self.guiapp.call(self.guiapp.window["games"].edit, "current", "state", "normal")
         else:
-            self.application.
+            self.guiapp.call(self.guiapp.window["load_game"].edit, "download", "text", "Failed")
+
+    async def _play_game(self):
+        if self._active.is_set():
+            return
+        self._active.set()
+        print("Playing Game")
+        await self.guiapp.acall(game.run, self.games.get().main)
+        print("Game Over")
+        self._active.clear()
+        self.guiapp.call(self.guiapp.window["games"].return_game)
 
 def main(addr: str):
     print("Client")
@@ -90,4 +185,6 @@ def main(addr: str):
     client.main()
 
 if __name__ == "__main__":
+    Interface.main_thread()
     main("127.0.0.1")
+    Interface.stop()
--- server.py
+++ server.py
@@ -1,27 +1,114 @@
-import time
 import db
-import network
+import node
+import loader
+import asyncio
+import website
+from interface import Interface
 
-Database = db.Database()
+class SClient(node.SClient):
 
-def main(repopulate=False):
+    async def open(self):
+        await super().open()
+        print("Connection:", self._node)
+
+    async def dsptch_glist(self, data: node.Data):
+        print("Request Game List")
+        glist = loader.list_avalible()
+        print(glist)
+        self.send(len(glist), "GLIST")
+        for element in glist:
+            self.send(element, node.Tag("GLIST"))
+
+    async def dsptch_game(self, data: node.Data):
+        print("Game Request:", data.data)
+        if filename := loader.find_file(data.data):
+            print("Game File:", data.data)
+            return self.send(loader.read(filename), "GAME")
+        print("Game Not Found:", data.data)
+        return self.send(False, "GAME")
+
+    async def dsptch_database(node: 'node.DataInterface', data: node.Data):
+        print("Database Request", data)
+
+class CSStyle(website.Request):
+    async def handle(self):
+        self.client.header << website.Header("Content-Type", "text/css")
+        self.client.header << website.Header("Cache-Control", "public, max-age=604800, immutable")
+        await self.load_style("/".join(self.request[1:]))
+
+    async def load_style(self, filename: str):
+        data = await website.buffer.File(website.path+"web/style/"+filename).compile()
+        if data.startswith(b"# meta"):
+            lines = data.splitlines(False)[1:]
+            for index, fname in enumerate(lines, start=1):
+                if fname:
+                    if fname.startswith(b"# meta"):
+                        self.client.buffer << b"\n".join(lines[index+1:])
+                        break
+                    await self.load_style(fname.decode())
+        else:
+            self.client.buffer << data
+
+class Server(node.Server):
+    # Get Database Referance
+    database = db.Database()
+
+    class WebRequest(website.Request):
+        inst = None
+        end = asyncio.Event()
+
+        async def handle(self):
+            await self.tree.traverse(self)
+
+        def kill(self):
+            if self.client.query.get("key") == "adminkill":
+                self.kill_flag = True
+                async def kill_timer():
+                    await Interface.next(1)
+                    self.end.set()
+                Interface.schedule(kill_timer())
+            self.client.buffer << website.buffer.Python(f"{website.path}web/page/kill.html", self)
+
+        tree = website.Tree(
+            (home := website.buffer.Python(f"{website.path}web/page/home.html")),
+            home, home,
+            kill=kill,
+            style=CSStyle,
+        )
+
+    def __init__(self):
+        self.WebRequest.inst = self
+        self.web = website.Server(self.WebRequest, port=610)
+        website.buffer.Buffer.cache_disable = True # DEBUG
+        super().__init__("", 609, 10, False, SClient, echo=node.dispatch.echo)
+
+    async def __aenter__(self):
+        await self.database.__aenter__()
+        await super().__aenter__()
+        await self.web.__aenter__()
+        return self
+    async def __aexit__(self, *args):
+        await self.web.__aexit__(*args)
+        await super().__aexit__(*args)
+        await self.database.__aexit__(*args)
+        return
+
+async def main(repopulate=False):
     print("Server")
     # Repopulate the Database
     if repopulate:
-        Database.new()
-        with Database:
-            Database.repopulate()
-            # Test Accounts
-            Database.register("Admin", "password")
+        Server.database.new()
+        async with Server.database:
+            await Server.database.repopulate()
 
-    server = network.Server()
+    server = Server()
+    #  Main Serving Loop
+    async with server:
+        await server.WebRequest.end.wait()
 
-    with Database, server:
-        try:
-            while True:
-                time.sleep(1)
-        except KeyboardInterrupt:
-            return
+    Interface.stop()
 
 if __name__ == "__main__":
-    main(repopulate=True)
+    from interface import Interface
+    Interface.schedule(main(repopulate=True))
+    Interface.main()

6ca498fbe671019fedfb92e361f4b8dfb926ff70
TWoolhouse <thomasrwoolhouse@gmail.com>
2021-03-30 10:13:59 +0100

    Game Dino: Use new Game interface and new Neuron Layout

--- games/g_dino/main.py
+++ games/g_dino/main.py
@@ -1,18 +1,11 @@
-from collections import deque
+from game import engine, GameApplication
 import enum
-import random
-from game import Game, engine
-
 import neural
-import pickle
-from path import PATH
+import random
+from collections import deque
+from interface import Interface
 
-# AI
-ITERATIONS = 7
-RENDER = True
-# AI
-
-FLOOR = Game.height // 2 + Game.height // 4
+FLOOR = GameApplication.height // 2 + GameApplication.height // 4
 
 class StateManager(engine.component.Script):
 
@@ -22,7 +15,6 @@ class StateManager(engine.component.Script):
         RUN = 1
         HIT = 2
         PAUSE = 3
-        UNPAUSE = 4
 
     def __init__(self):
         self.state = self.GameState.NONE
@@ -36,34 +28,34 @@ class StateManager(engine.component.Script):
         engine.instantiate(
             engine.component.Render(self._text_score, True),
             parent=self.entity,
-            transform=engine.component.Transform(engine.Vector(Game.width - 20, 32))
+            transform=engine.component.Transform(engine.Vector(GameApplication.width - 20, 32))
         )
         self._text_high = engine.render.Text(0)
         engine.instantiate(
             engine.component.Render(self._text_high, True),
             parent=self.entity,
-            transform=engine.component.Transform(engine.Vector(Game.width - 20, 20))
+            transform=engine.component.Transform(engine.Vector(GameApplication.width - 20, 20))
         )
 
-        self._text_id = engine.render.Text(engine.app().program.game.id)
-        engine.instantiate(
-            engine.component.Render(self._text_id, True),
-            parent=self.entity,
-            transform=engine.component.Transform(engine.Vector(Game.width - 20, 44))
-        )
+        # self._text_id = engine.render.Text(engine.app().program.id)
+        # engine.instantiate(
+        #     engine.component.Render(self._text_id, True),
+        #     parent=self.entity,
+        #     transform=engine.component.Transform(engine.Vector(GameApplication.width - 20, 44))
+        # )
 
         self._text_net_out = engine.render.Text([0, 0, 0])
         engine.instantiate(
             engine.component.Render(self._text_net_out, True),
             parent=self.entity,
-            transform=engine.component.Transform(engine.Vector(Game.width - 300, 50))
+            transform=engine.component.Transform(engine.Vector(GameApplication.width - 300, 50))
         )
 
         self._text_net = engine.render.Text("NET")
         engine.instantiate(
             engine.component.Render(self._text_net, True),
             parent=self.entity,
-            transform=engine.component.Transform(engine.Vector(Game.width - 300, 70))
+            transform=engine.component.Transform(engine.Vector(GameApplication.width - 300, 70))
         )
 
         self.state = self.GameState.RUN
@@ -77,12 +69,9 @@ class StateManager(engine.component.Script):
         if self.state is self.GameState.HIT:
             self.obstacle.collide()
             self.state = self.GameState.PAUSE
-        if self.state is self.GameState.UNPAUSE:
-            self.reset()
-            self.state = self.GameState.RUN
 
     def collide(self):
-        if self.state is self.GameState.RUN:
+        if self.state is self.GameState.RUN and (self.score - engine.core.DeltaTime().dt() * 20) >= 1:
             self.state = self.GameState.HIT
 
     def reset(self):
@@ -93,10 +82,13 @@ class StateManager(engine.component.Script):
         if event.dispatch(engine.event.KeyPress) and event.key is engine.input.Key.R:
             if self.state is self.GameState.PAUSE:
                 self.reset()
-                self.state = self.GameState.UNPAUSE
+                self.state = self.GameState.RUN
 
 @engine.ecs.require(engine.component.Collider)
 class Obstacle(engine.ecs.Component):
+
+    def __init__(self, width, height):
+        self.width, self.height = width, height
 
     def initialize(self):
         self.transform = self.Get(engine.component.Transform)
@@ -110,10 +102,11 @@ class ObstacleManager(engine.component.Script):
     RATE = 0.01
     AREA = 8000
     WIDTH = (40, 160)
+    HEIGHT = AREA // WIDTH[0]
 
-    _offset = -Game.width // 4
-    _spawn = 2 * Game.width
-    __range = Game.width // 4 + Game.width // 6
+    _offset = -GameApplication.width // 4
+    _spawn = 2 * GameApplication.width
+    __range = GameApplication.width // 4 + GameApplication.width // 6
 
     def __init__(self):
         self.children = deque()
@@ -162,10 +155,10 @@ class ObstacleManager(engine.component.Script):
         engine.app().world.destroy(self.children.popleft().entity)
 
     def add(self):
-        self.dist = random.randint(self.__range, Game.width)
-        obstacle = Obstacle()
+        self.dist = random.randint(self.__range, GameApplication.width)
         width = random.randint(*self.WIDTH)
         dim = engine.Vector(width, self.AREA // width)
+        obstacle = Obstacle(*dim)
         engine.instantiate(
             obstacle,
             engine.component.Collider(engine.physics.collider.Rectangle, engine.component.Transform(scl=dim), "Obstacle"),
@@ -276,74 +269,69 @@ class PlayerControllerAI(engine.component.Script):
     def __init__(self, manager: StateManager):
         self.manager = manager
         # Inputs:
-        # Obstacles - Value
-        # Player - Y, YVel
-        # Obs_Closest - X, Width, Height
-        # Obs_Next - X, Width, Height
-        # Obs_3 - X, Width, Height
-        self.network = neural.Network(neural.layout.FeedForward(1, 3, *[15]*15))
-        # Nothing, Jump, Fall
+        # Player - Y
+        # i = range(2)
+        # Obs[i] - X, Height
+        self.network = Application.AI
+        # Output: Jump, Fall
+
+        self.iteration, self.score = 0, 0
 
     def initialize(self):
         super().initialize()
         self.controller = self.Get(PlayerController)
-        self._load_net()
-        # self._aglo = neural.algorithm.Genetic(self.network)
-        # self.network = next(self._aglo.train(ITERATIONS, 2))
+        self.network = engine.app().program.AI
+        engine.app().window.title(f"Dinosaur - {self.iteration}")
 
     def terminate(self):
-        self._save_net()
-
-    def _load_net(self):
-        try:
-            with open(PATH+f"games/dino/fall.net", "rb") as file:
-                self.network = pickle.load(file)
-        except FileNotFoundError:
-            pass
-    def _save_net(self):
-        # with open(PATH+f"games/dino/{engine.app().program.game.id}.new.net", "wb") as file:
-        #     pickle.dump(self.network, file)
-        pass
+        if self.iteration < engine.app().program.iterations:
+            engine.app().program.fitness = None
+        else:
+            engine.app().program.fitness = self.score / self.iteration
 
     def update(self):
-        data = []
-        try:
-            child = self.manager.obstacle.children[0]
-            data.append(neural.maths.constrain(child.transform.position[0], ObstacleManager._offset, ObstacleManager._spawn))
-        except IndexError:
-            data.append(1)
-        out = self.network.input(*data)
+        height = neural.maths.constrain(self.controller.transform.position_global[1], FLOOR, 0)
+        data = [height] * 5
+        # data[1] = neural.maths.constrain(self.manager.obstacle.speed, 1, 100)
+        for i in range(2):
+            try:
+                child = self.manager.obstacle.children[i]
+                data[2*i+1] = neural.maths.constrain(child.transform.position[i], ObstacleManager._offset, ObstacleManager._spawn)
+                data[2*i+2] = child.height // self.manager.obstacle.HEIGHT
+            except IndexError:
+                data[2*i+1] = 1
+                data[2*i+2] = 0
+        out = self.network.feed(*data)
         self.manager._text_net_out.text = out
-        com = out.index(max(out))
-        self.manager._text_net.text = com
-        if com == 1:
-            self.controller.jump()
-        elif com == 2:
-            self.controller.fall()
+        self.network.result(out, self.controller.jump, self.controller.fall)
+        self.manager._text_net.text = out.index(max(out))
 
     def fail(self):
         if self.manager.state is self.manager.GameState.PAUSE:
-            # try:
-            #     title = engine.app().window._master.title().split("-")
-            #     iteration = int(title[-1])
-            #     print(f"ID<{engine.app().program.game.id}> {iteration}: {int(self.manager.score)}")
-            #     new_title = "-".join(title[:-1]) + f"-{iteration+1}"
-            #     self._aglo.fitness(self.manager.score)
-            #     self.network = next(self._aglo._iter)
-            #     engine.app().window._master.title(new_title)
-            # except StopIteration:
-            #     self.network = self._aglo.merge()
-            #     engine.app().event(engine.event.KeyPress(engine.input.Key.ESCAPE))
-            engine.app().event(engine.event.KeyPress(engine.input.Key.R))
+            self.iteration += 1
+            self.score += self.manager.score
+            app = engine.app()
+            app.window.title(f"Dinosaur - {self.iteration}")
+            if self.iteration >= app.program.iterations:
+                return app.event(engine.event.KeyPress(engine.input.Key.ESCAPE))
+            app.event(engine.event.KeyPress(engine.input.Key.R))
 
-class Application(Game):
+class Application(GameApplication):
 
-    def initialize(self):
-        app = engine.app()
+    AI = neural.Network(neural.layout.FeedForward(
+        (5, neural.neuron.Input),
+        (2, neural.neuron.Output),
+        *[(3, neural.neuron.Hidden)] * 1,
+        *[(3, neural.neuron.Recurrent)] * 3,
+        *[(3, neural.neuron.Hidden)] * 1,
+    ))
+
+    def initialize(self, app: engine.core.Application):
+        super().initialize(app)
         engine.instantiate(engine.component.Event(self.close_event, "WINDOW"), id=False)
 
-        app.window._master.title(f"Dino-{app.program.game.id}-0")
-        app.world.system(engine.ecs.systems.Render, RENDER)
+        app.window._master.title(f"Dinosaur")
+        app.world.system(engine.ecs.systems.Render, True)
         app.world.add_system(PhysBodySystem(), "PHYSICS")
         app.world.add_system(engine.ecs.systems.FPS(), "POST")
 
@@ -367,25 +355,24 @@ class Application(Game):
         )
 
         engine.instantiate( # Player
-            PlayerControllerInput(),
-            PlayerControllerAI(manager),
             PlayerController(manager),
+            PlayerControllerAI(manager) if self.AIState >= self.AIState.ACTIVE else PlayerControllerInput(),
             PhysBody(0.06),
             engine.component.Collider(engine.physics.collider.Rectangle, engine.component.Transform(scl=engine.Vector(50, 100)), "Player"),
             engine.component.Render(engine.render.Polygon.Quad(50, 100), layer="Player"),
-            transform=engine.component.Transform(engine.Vector(Game.width // 16, Game.height // 2))
+            transform=engine.component.Transform(engine.Vector(GameApplication.width // 16, GameApplication.height // 2))
         )
 
         engine.instantiate( # Floor
-            engine.component.Collider(engine.physics.collider.Rectangle, engine.component.Transform(scl=engine.Vector(Game.width, 20)), "Enviroment"),
-            engine.component.Render(engine.render.Polygon.Quad(Game.width, 20)),
-            transform=engine.component.Transform(engine.Vector(Game.width // 2, FLOOR + 10))
+            engine.component.Collider(engine.physics.collider.Rectangle, engine.component.Transform(scl=engine.Vector(GameApplication.width, 20)), "Enviroment"),
+            engine.component.Render(engine.render.Polygon.Quad(GameApplication.width, 20)),
+            transform=engine.component.Transform(engine.Vector(GameApplication.width // 2, FLOOR + 10))
         )
 
         engine.instantiate( # FPS
             engine.component.FPS(),
             engine.component.Render(engine.render.Text("FPS"), True),
-            transform=engine.component.Transform(engine.Vector(Game.width - 20, 8))
+            transform=engine.component.Transform(engine.Vector(GameApplication.width - 20, 8))
         )
 
     def close_event(self, event: engine.event.KeyPress):
